8cb0d82b2450550fab4a4625179992f1d1079508
Audrey Zhou <auddog19@gmail.com>
2023-11-30 19:00:59 -0800

    Enable driving using xbox controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,8 +6,10 @@ package frc.robot;
 
 import frc.robot.Constants.RobotConstants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.DriveUsingController;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -21,6 +23,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  private final SwerveSubsystem m_driveTrain = new SwerveSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandXboxController m_driverController =
@@ -28,6 +31,8 @@ public class RobotContainer {
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    m_driveTrain.setDefaultCommand(new DriveUsingController(m_driveTrain, m_driverController));
+    
     // Configure the trigger bindings
     configureBindings();
   }
